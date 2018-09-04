4955ded7a99adf00ef9b85bf4be5468f8d845b91
Christopher Arnold <chrisarnold2010@gmail.com>
2018-09-04 14:03:34 -0500

    lacking likes functionality

--- src/index.js
+++ src/index.js
@@ -4,23 +4,49 @@ let addToy = false
 
 // YOUR CODE HERE
 
-const url = 'http://localhost:3000/toys'
-const divId = document.getElementById.('toy-collection')
-function createNode(element) {
-   return document.createElement(element); // Create the type of element you pass in the parameters
- }
-function append(parent, el) {
-   return parent.appendChild(el); // Append the second parameter(element) to the first one
- }
+const parseJSON = response => response.json()
 
-fetch(url)
-	.then((response) => response.json())
-		.then(function(data) {
-			let toys = data.results;
-			return toys.map(function(toy) {
-				let div = createNode('div'),
-			})
-  	});
+function putToysOnPage(toys) {
+	const toyCollectionDiv = document.getElementById('toy-collection')
+	// const toyCollectionDiv = document.querySelector('#toy-collection')
+	toys.forEach(function(toy) {
+		toyCollectionDiv.innerText += `
+			<div class="card">
+				<h2>${toy.name}</h2>
+				<img src = "${toy.image}" class="toy-avatar">
+				<p>${toy.likes} Likes <p>
+          		<button class="like-btn">Like <3</button>
+			</div
+		`
+	})
+}
+
+fetch('http://localhost:3000/toys')
+	.then(parseJSON)
+	.then(putToysOnPage)
+
+
+// new toy submission
+const addToyForm = document.querySelector('.add-toy-form')
+
+// const addToyForm = document.getElementByClassName('add-toy-form')[0]
+addToyForm.addEventListener('submit', function(event) {
+	event.preventDefault();
+	// console.log('form-submitted')
+	fetch('http://localhost:3000/toys', {
+		method: 'POST',
+		headers: {
+			'Content-Type' : 'application/json'
+		},
+		body: JSON.stringfy({
+  			"name": "Jessie",
+  			"image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+  			"likes": 0
+		})
+	})
+	// .then(parseJSON)
+	// .then(console.log)
+})
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -35,3 +61,33 @@ addBtn.addEventListener('click', () => {
 
 
 // OR HERE!
+
+
+
+
+
+
+
+
+
+
+// THIS IS ALL MY OLD CODE
+
+// const url = 'http://localhost:3000/toys'
+// const divId = document.getElementById.('toy-collection')
+// function createNode(element) {
+//   return document.createElement(element); // Create the type of element you pass in the parameters
+// }
+// function append(parent, el) {
+//   return parent.appendChild(el); // Append the second parameter(element) to the first one
+// }
+
+// fetch(url)
+// 	.then((response) => response.json())
+// 		.then(function(data) {
+// 			let toys = data.results;
+// 			return toys.map(function(toy) {
+// 				let div = createNode('div'),
+// 			})
+//   	});
+
