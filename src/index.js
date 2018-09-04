5da4eac40b2ffad8c75c13df0351bc679b427399
Christopher Arnold <chrisarnold2010@gmail.com>
2018-09-04 16:46:10 -0500

    need likes

--- src/index.js
+++ src/index.js
@@ -2,22 +2,21 @@ const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
 
-// YOUR CODE HERE
-
-const parseJSON = response => response.json()
+// add the toys to the page
+const parseJSON = resp => resp.json()
 
 function putToysOnPage(toys) {
 	const toyCollectionDiv = document.getElementById('toy-collection')
 	// const toyCollectionDiv = document.querySelector('#toy-collection')
 	toys.forEach(function(toy) {
-		toyCollectionDiv.innerText += `
-			<div class="card">
-				<h2>${toy.name}</h2>
-				<img src = "${toy.image}" class="toy-avatar">
-				<p>${toy.likes} Likes <p>
-          		<button class="like-btn">Like <3</button>
-			</div
-		`
+		toyCollectionDiv.innerHTML += `
+            <div class="card">
+              <h2>${toy.name}</h2>
+              <img src="${toy.image}" class="toy-avatar">
+              <p>${toy.likes} Likes<p>
+              <button class="like-btn">Like <3</button>
+            </div>
+        `
 	})
 }
 
@@ -25,27 +24,25 @@ fetch('http://localhost:3000/toys')
 	.then(parseJSON)
 	.then(putToysOnPage)
 
-
 // new toy submission
 const addToyForm = document.querySelector('.add-toy-form')
-
-// const addToyForm = document.getElementByClassName('add-toy-form')[0]
+// const addToyForm = document.getElementsByClassName('add-toy-form')[0]
 addToyForm.addEventListener('submit', function(event) {
-	event.preventDefault();
-	// console.log('form-submitted')
-	fetch('http://localhost:3000/toys', {
-		method: 'POST',
-		headers: {
-			'Content-Type' : 'application/json'
-		},
-		body: JSON.stringfy({
-  			"name": "Jessie",
-  			"image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
-  			"likes": 0
-		})
-	})
-	// .then(parseJSON)
-	// .then(console.log)
+  event.preventDefault();
+
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "name": "Jessie",
+      "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+      "likes": 0
+    })
+  })
+    .then(parseJSON)
+    .then(console.log)
 })
 
 addBtn.addEventListener('click', () => {
