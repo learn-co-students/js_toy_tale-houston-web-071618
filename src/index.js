46bf9bab6784e0da8ffd92ec8a1bdf5b9f9ef796
Christopher Arnold <chrisarnold2010@gmail.com>
2018-09-04 07:32:17 -0500

    fetch method progressing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ let addToy = false
 
 // YOUR CODE HERE
 
+const url = 'http://localhost:3000/toys'
+const divId = document.getElementById.('toy-collection')
+function createNode(element) {
+   return document.createElement(element); // Create the type of element you pass in the parameters
+ }
+function append(parent, el) {
+   return parent.appendChild(el); // Append the second parameter(element) to the first one
+ }
+
+fetch(url)
+	.then((response) => response.json())
+		.then(function(data) {
+			let toys = data.results;
+			return toys.map(function(toy) {
+				let div = createNode('div'),
+			})
+  	});
+
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
